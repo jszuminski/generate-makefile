09a0ad079811de05f976f425b8c8585f8956a676
jszuminski <jakubszuminski0@gmail.com>
2025-10-26 12:54:10 +0100

    check for project dir permissions

--- generate_makefile.sh
+++ generate_makefile.sh
@@ -17,6 +17,7 @@ if [[ $# -ne 1 ]]; then
 fi
 
 # check if the directory exists
+project_dir=$1
 if [[ ! -d "$project_dir" ]]; then
     echo "Error: the directory '$project_dir' does not exist or is not a directory." >&2
     exit 2
@@ -24,9 +25,25 @@ fi
 
 echo "The given directory is correct, continuing..."
 
+# check read permission
+if [[ ! -r "$project_dir" ]]; then
+    echo "Error: no read permission for directory '$project_dir'." >&2
+    exit 3
+fi
+
+# check write permission
+if [[ ! -w "$project_dir" ]]; then
+    echo "Error: no write permission for directory '$project_dir'." >&2
+    exit 4
+fi
+
+echo "Directory is accessible (read & write)."
+
+
 # My own notes
 # >&2 -> redirects output to stderr (>&1 is just stdout)
 # exit 0 - success
 # exit 1 - general error (something failed)
 # exit 2 - misuse of shell builtins or wrong usage (ex. wrong arguments)
 # exit 3+ - custom error codes (own logic)
+# there can be no spaces between assignement to a variable (var=$1 not var = $1)
